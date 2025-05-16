40f0610edae6c6e27a44b0c3515838c9b63611a1
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-15 19:16:49 -0500

    Nix format

--- flake.nix
+++ flake.nix
@@ -1,20 +1,18 @@
 {
-	description = "AMALTHEA NixOS Flake";
+  description = "AMALTHEA NixOS Flake";
 
-	inputs = {
-		# Pinning nixpkgs here - they dropped support for the
-		# old pi hardware config in this version.
-		nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
-                nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-	};
+  inputs = {
+    # Pinning nixpkgs here - they dropped support for the
+    # old pi hardware config in this version.
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+  };
 
-	outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: rec {
-		nixosConfigurations."jupiter" = nixpkgs.lib.nixosSystem {
-			system = "aarch64-linux";
-			modules = [
-                                nixos-hardware.nixosModules.raspberry-pi-4
-				./configuration.nix
-			];
-		};
-	};
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: rec {
+    nixosConfigurations."jupiter" = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules =
+        [ nixos-hardware.nixosModules.raspberry-pi-4 ./configuration.nix ];
+    };
+  };
 }
