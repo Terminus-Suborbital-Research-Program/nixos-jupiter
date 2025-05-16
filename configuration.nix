b53f49a44d990b3eee5d1d5ab51c537ba20bde10
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-16 02:31:05 +0000

    Added uart maybe

--- configuration.nix
+++ configuration.nix
@@ -7,13 +7,11 @@
 
   system.stateVersion = "24.11"; # Pinned, DON"T CHANGE
 
-  boot.kernelParams = [ "console=tty1" ];
-
-  systemd.services."serial-getty@ttyAMA0".enable = false;
-  systemd.services."serial-getty@ttyS0".enable   = false;
-
-  hardware.raspberry-pi."4".i2c1.enable = true;
-  hardware.rasbperry-pi."4".bluetooth.enable = false;
+  boot.kernelParams = [
+    "console=tty1"
+    "8250.nr_uarts=1"
+    "console=serial0,115200n8"
+  ];
 
   # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
   boot.loader.grub.enable = false;
