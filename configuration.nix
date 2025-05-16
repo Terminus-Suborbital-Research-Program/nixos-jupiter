ac25d979426d79b0087cfc733dafdfd9c930bc49
Lucas Thelen <89479728+thelenlucas@users.noreply.github.com>
2025-05-16 18:13:36 -0500

    Added michael to authorized users

--- configuration.nix
+++ configuration.nix
@@ -49,6 +49,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/QIvKf8yNyDwfBHuyoL9lrhnewB9FO+33SnxyoD+AJ lucas@nixos"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFXPxcaJrD7Lu2P1/CxCwoKySNrszKuXgJteVZFo9vk3 supergoodname77@cachyos-x8664"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCX4xbbfhNuS6F5cb1dwHMmi+0T5B+Tea8QVfiYP5Ncia5J+BiMFxEH9S49QY9kpln3FchKxhWerUeftbW2gGUkdICIdjyEjShNYXHsSQoqvxlhaSvxJ5NsDpsu+kR1PoSrHmWyUcea5Bqy6S3lLEb5EcOzNjhuquJCoDRgRZQHNaPDHz3i+92bIAWAosu+mMkEa5/SxIxAh/6z+P3loHshtYWdEsMupOlX5MxHfXOepFVN7KTX36Rrqao198TSc0oik9JIOMUlmTc/EaU4PvXHI5I3T43JXOpFTLrJWkUGpPbUOaNzz7yOYqr2hvClA9aOzJIXtYWTcagQqFI2+VsstvNA1ci6/0UVhtBxuH1vs5F5P8IM6fScs9WCu4TE2rEDThzfrHpCuLWFhsYV+5dxGXEm+RrJmroUpelnm67CrNWgqgQPqJq55MZi0hHoxOXiFd/FyLwmULhajIfRJjkm2S1zbXAHUQ2Wju6mV2YrMpNlkpURKRcNtJx4OqLa1wE= bigmeech@hephaestus"
     ];
   };
 
