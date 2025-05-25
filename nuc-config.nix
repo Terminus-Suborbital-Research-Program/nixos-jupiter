f982bb87438c50389d328ad935ff6a89662a29d2
= <=>
2025-05-25 02:39:55 +0000

    Disable poor networking

--- nuc-config.nix
+++ nuc-config.nix
@@ -18,11 +18,6 @@
     [ rtl88xxau-aircrack ];
 
   networking.hostName = "nuc"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable =
-    true; # Easiest to use and most distros use this by default.
-  networking.networkmanager.wifi.powersave = false;
 
   #  # Set your time zone.
   time.timeZone = "US/Chicaco";
