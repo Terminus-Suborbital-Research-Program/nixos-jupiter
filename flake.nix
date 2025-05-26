53deb5b1b4ac71a7f7459d7ec6a95d3f0413b342
Lucas Thelen <thelenlucas028@gmail.com>
2025-05-25 23:17:32 -0500

    Added radiacode stuff

--- flake.nix
+++ flake.nix
@@ -6,11 +6,13 @@
     # old pi hardware config in this version.
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    guard.url = "github:Terminus-Suborbital-Research-Organization/GUARD";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: rec {
-    nixosConfigurations."jupiter" = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
+  outputs = { self, nixpkgs, nixos-hardware, guard, ... }@inputs: rec {
+    nixosConfigurations."jupiter" = let system = "aarch64-linux";
+    in nixpkgs.lib.nixosSystem {
+      inherit system;
       modules = [
         nixos-hardware.nixosModules.raspberry-pi-4
         ./configuration.nix
@@ -18,15 +20,19 @@
         ./modules/user.nix
         ./modules/wireless.nix
       ];
+
+      configuration = {
+        environment.systemPackages = [ guard.packages.${system}.radiaread ];
+      };
     };
 
-    nixosConfigurations."nuc" = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./nuc-config.nix
-        ./modules/programs.nix
-        ./modules/user.nix
-      ];
+    nixosConfigurations."nuc" = let system = "x86_64-linux";
+    in nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [ ./nuc-config.nix ./modules/programs.nix ./modules/user.nix ];
+      configuration = {
+        environment.systemPackages = [ guard.packages.${system}.radiaread ];
+      };
     };
   };
 }
