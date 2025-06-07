b309ff0c58d1bcc75dedd92336adee2ddcdde1e3
Lucas Thelen <thelenlucas028@gmail.com>
2025-06-07 12:20:13 -0500

    Stuff that got lost?

--- flake.nix
+++ flake.nix
@@ -7,15 +7,14 @@
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     guard.url = "github:Terminus-Suborbital-Research-Program/GUARD";
-    infratracker.url =
-      "github:Terminus-Suborbital-Research-Program/COTS-Star-Tracker-Amalthea";
     jupiter.url = "github:Terminus-Suborbital-Research-Program/AMALTHEA";
   };
 
-  outputs = { nixpkgs, nixos-hardware, guard, infratracker, jupiter, ... }: {
+  outputs = { nixpkgs, nixos-hardware, guard, jupiter, ... }: {
     nixosConfigurations."jupiter" = let
       system = "aarch64-linux";
       jupiter-pkg = jupiter.packages.${system}.jupiter-fsw;
+      pkgs = import nixpkgs { inherit system; };
     in nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
@@ -26,17 +25,13 @@
         ./modules/wireless.nix
         ./modules/lsm6dt.nix
         {
-          environment.systemPackages = [
-            guard.packages.${system}.radiaread
-            infratracker.packages.${system}.infratracker
-            jupiter-pkg
-          ];
+          environment.systemPackages =
+            [ guard.packages.${system}.radiaread jupiter-pkg ];
 
           # ensure the data dir exists at boot
           systemd.tmpfiles.rules = [
             # format: TYPE PATH MODE OWNER GROUP AGE ARGUMENT
             "d /home/terminus/rad_data 0755 terminus terminus - -"
-            "d /home/terminus/infratracker_data 0755 terminus terminus - -"
           ];
 
           systemd.services.radiaread = {
@@ -62,6 +57,7 @@
           systemd.services.jupiter = {
             description = "JUPITER Flight software";
             after = [ "systemd-tmpfiles-setup.service" ];
+            path = [ jupiter-pkg pkgs.libgpiod ];
 
             serviceConfig = {
               WorkingDirectory = "/home/terminus/";
@@ -71,23 +67,6 @@
               RestartSec = "2s";
               User = "terminus";
             };
-          };
-
-          systemd.services.infratracker = {
-            description = "Terminus Infratracker Daemon";
-            after = [ "systemd-tmpfiles-setup.service" ];
-
-            serviceConfig = {
-              WorkingDirectory = "/home/terminus/infratracker_data";
-              ExecStart = "${
-                  infratracker.packages.${system}.infratracker
-                }/bin/infratracker .";
-              Restart = "always";
-              RestartSec = "10s";
-              Group = "dialout";
-            };
-
-            wantedBy = [ "multi-user.target" ];
           };
         }
       ];
