590c31bfe7d8bc9256cb2ddeb39b95ac2fafa925
Lucas Thelen <thelenlucas028@gmail.com>
2025-06-09 14:01:48 -0500

    Added disables for the bluetooth

--- modules/wireless.nix
+++ modules/wireless.nix
@@ -4,4 +4,9 @@
     networks."Staff5".pskRaw =
       "66fe08674eda745336a1ac1dddf2e7fef7d1374a6c73184194a05332e0648ff1";
   };
+
+  # Disable bluetooth
+  boot.blackListedKernelModules =
+    [ "bluetooth" "btusb" "btbcm" "btrtl" "btintel" "hci_uart" ];
+  hardware.bluetooth.enable = false;
 }
